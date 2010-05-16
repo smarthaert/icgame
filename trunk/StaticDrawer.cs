866834ee6cb38013b0bbc1a2f9d1313c01067b16
cmilucmilu@gmail.com <cmilucmilu@gmail.com@a7c8c05b-20ef-c93c-1ef3-b22966dc7052>
2010-05-16 14:22:25 +0000

    Nowe klasy

---
+++ trunk/StaticDrawer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ICGame
+{
+    public class StaticDrawer : GameObjectDrawer
+    {
+        public StaticDrawer(GameObject gameObject)
+            : base(gameObject)
+
+        {
+        }
+    }
+}
